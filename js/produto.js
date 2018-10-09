0f440437d1e2cb227e6f78b39ec7698aaee99d58
SamaraFerC <samara.fe.candido@gmail.com>
2018-10-09 12:41:52 -0300

    09/10.04s
    Utilizei jquery na home, adcionei jquery em todos os arquivoss.
    Mostrar e esconder as figuras deve ser arrumado, pois não funcionou
    pg218

--- js/produto.js
+++ js/produto.js
@@ -1,7 +1,12 @@
 var	inputTamanho	=	document.querySelector('[name=tamanho]');
 var	outputTamanho	=	document.querySelector('[name=valortamanho]');
+inputTamanho.oninput	=	mostraTamanho;
+inputTamanho.onchange	=	mostraTamanho;
+
+	
 	function	mostraTamanho(){
 		outputTamanho.value	=	inputTamanho.value;
-	}
+		outputTamanho.textContent	=	event.target.value
+	};
 
-inputTamanho.oninput =	mostraTamanho;
+inputTamanho.oninput =	mostraTamanho
